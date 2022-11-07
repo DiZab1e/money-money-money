aa63cb3b1595b93a7953804c39ce513d086ee080
Григорий <fash1on.pro@yandex.ru>
2022-11-07 10:56:40 +0300

    mk upds

---
+++ money, money, money.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Kata
+{
+    public static Int32 CalculateYears(Double Principal, Double Interest, Double Tax, Double DesiredPrincipal)
+    {
+        var years = 0;
+        while (Principal < DesiredPrincipal)
+        {
+            Principal += Principal * Interest * (1 - Tax);
+            years++;
+        }
+        return years;
+    }
+}
